56d8c56b15c5964d171bf3b02290101219b04079
Winifred Au <winifredau1990@gmail.com>
2015-08-11 12:46:44 -0400

    can now filter by tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@ class Tag
   include DataMapper::Resource
   property :id, Serial
   property :name, String
+  has n, :links, through: Resource
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -6,6 +6,13 @@ feature "Viewing links" do
     click_link "View links"
     expect(current_path).to eq "/links"
   end
+
+  before(:each) do
+    Link.create(url: "http://www.makersacademy.com", title: "Makers Academy", tags: [Tag.first_or_create(name: "education")])
+    Link.create(url: "http://www.google.com", title: "Google", tags: [Tag.first_or_create(name: "search")])
+    Link.create(url: "http://www.zombo.com", title: "This is Zombocom", tags: [Tag.first_or_create(name: "bubbles")])
+    Link.create(url: "http://www.bubble-bobble.com", title: "Bubble Bobble", tags: [Tag.first_or_create(name: "bubbles")])
+  end
   scenario "I can see existing links on the links page" do
     link = Link.new(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     link.save
@@ -22,5 +29,15 @@ feature "Viewing links" do
     click_button 'Create link'
     click_link 'http://www.zombo.com/'
     expect(current_url).to eq 'http://www.zombo.com/'
+  end
+
+  scenario 'I can filter links by tag' do
+    visit '/tags/bubbles'
+    within 'ul#links' do
+      expect(page).not_to have_content("Makers Academy")
+      expect(page).not_to have_content("Code.org")
+      expect(page).to have_content("This is Zombocom")
+      expect(page).to have_content("Bubble Bobble")
+    end
   end
 end
