a4dc2eb9b543e2bb851c513f98fd0db068053954
Josh <Tattsy@hotmail.co.uk>
2015-08-11 12:30:49 +0100

    links now connects to database

--- app/bookmark_manager_web.rb
+++ app/bookmark_manager_web.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 require_relative "../data_mapper_setup"
+require_relative "./models/link"
+
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Hello BookmarkManager!'
+  end
+
+  get "/links" do
+    @links = Link.all
+    erb :index
   end
 
   # start the server if ruby file executed directly
