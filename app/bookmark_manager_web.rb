7dfed6b6457a9fd5cd87e50a6cf47614641bbdb1
Winifred Au <winifredau1990@gmail.com>
2015-08-11 08:54:38 -0400

    can now create links

--- app/bookmark_manager_web.rb
+++ app/bookmark_manager_web.rb
@@ -12,6 +12,15 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
+  get '/links/new' do
+    erb :link
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
