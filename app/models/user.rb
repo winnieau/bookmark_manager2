f22c132fa29e30a5d3b464af15627ba84f437f27
Josh <Tattsy@hotmail.co.uk>
2015-08-13 11:28:40 +0100

    now ask for user confirmation of password before sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 require "bcrypt"
 
 class User
+  attr_reader :password
+  attr_accessor :password_confirmation
+
   include DataMapper::Resource
+
+  validates_confirmation_of :password
 
   property :id, Serial
   property :email, String
@@ -9,6 +14,7 @@ class User
   property :password_digest, Text
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,12 +6,17 @@ feature "user sign up" do
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")
   end
-  
-  def sign_up(email: "alice@example.com", password: "oranges!")
+
+  scenario "requires a matching confirmation" do
+    expect { sign_up(password_confirmation: "wrong") }.not_to change(User, :count)
+  end
+
+  def sign_up(email: "alice@example.com", password: "oranges!", password_confirmation: "oranges!")
     visit "/users/new"
     expect(page.status_code).to eq 200
     fill_in :email, with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button "Sign up"
   end
 
