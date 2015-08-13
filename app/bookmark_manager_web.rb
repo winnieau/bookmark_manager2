f99efa0dab490d9b9ea7b73178b602845358a26a
Josh <Tattsy@hotmail.co.uk>
2015-08-13 07:05:10 +0100

    layout now works

--- app/bookmark_manager_web.rb
+++ app/bookmark_manager_web.rb
@@ -7,14 +7,11 @@ require_relative "./models/user"
 
 class BookmarkManager < Sinatra::Base
 
-  set :views, proc { File.join(root, 'views') }
-
   enable :sessions
   set :session_secret, "super secret"
 
   get '/' do
-    'Hello BookmarkManager!'
-    erb :home, :layout => true
+    erb :home
   end
 
   get "/links" do
@@ -51,15 +48,14 @@ class BookmarkManager < Sinatra::Base
   end
 
   post "/users" do
-    user = User.create(email: params[:email], 
-        password: params[:password])
+    user = User.create(email: params[:email], password: params[:password])
     session[:user_id] = user.id
     redirect to("/")
   end
 
   helpers do
     def current_user
-      User.get(id: session[:user_id])
+      User.get(session[:user_id])
     end
   end
 
