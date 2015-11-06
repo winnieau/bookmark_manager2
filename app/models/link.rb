a2f1a793c233dd72344c4a201ef22eb453c42355
Winifred Au <winifredau1990@gmail.com>
2015-11-06 11:43:11 -0500

    removing comments from link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,10 +1,8 @@
 class Link
 
-  # adds datamapper functionality to this class
   include DataMapper::Resource
 
-  # these property declarations set the column headers in the Link table
-  property :id,     Serial # Serial means that it will be auto-incremented for every record
+  property :id,     Serial
   property :title,  String
   property :url,    String
   has n, :tags, through: Resource
